816b4d4df9ad58dc1919d191bd87622adeff5827
xdjavit0 <99995052+xdjavit0@users.noreply.github.com>
2023-01-19 11:36:15 +0100

    Add files via upload

---
+++ src/Components/CardOfANew/Moks/Mock.js
@@ -0,0 +1,7 @@
+const data =
+{
+  title: 'Carlos le pego a marcos',
+  description: 'Carlos le partio la cara a marcos',
+  photo: 'https://d500.epimg.net/cincodias/imagenes/2022/07/07/legal/1657184420_702589_1657184542_noticia_normal.jpg'
+}
+export default data
