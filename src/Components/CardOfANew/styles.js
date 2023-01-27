5d333283a8c5e2bdb985b6c13d87ae7b96d675da
Luciano Ezequiel Mallo Tomaso <malloluciano@gmail.com>
2023-01-27 09:30:45 +0100

    style of the cards applied

--- src/Components/CardOfANew/styles.js
+++ src/Components/CardOfANew/styles.js
@@ -4,10 +4,33 @@ import { withTheme } from '../../helpers/styles.helper'
 export const Frame = styled.div`
 width: fit-content;
 height: fit-content;
+box-sizing: border-box;
+background: rgba(217, 217, 217, 0.58);
+border: 1px solid white;
+box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.22);
+backdrop-filter: blur(6px);
+text-align: center;
+cursor: pointer;
+transition: all 0.5s;
+display: flex;
+align-items: center;
+justify-content: center;
+user-select: none;
+font-weight: bolder;
 display: flex; 
 ${withTheme('margin-left', 'sizes.rem02')}
+${withTheme('margin-bottom', 'sizes.rem05')}
 ${withTheme('padding', 'sizes.rem02')}
 ${withTheme('background-color', 'colors.sand')}
+:hover{
+   
+        border: 1px solid black;
+        transform: scale(1.05);
+      
+}
+:active {
+    transform: scale(0.95) rotateZ(1.7deg);
+  }
 `
 export const PhotoOfTheNew = styled.img`
 display: inline-block; 
